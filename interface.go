376f6608515297e012c947343d40fa013b462ac8
Eitan Joffe <ejoffe@gmail.com>
2022-02-02 11:43:08 -0800

    Add support for different id types
    Currently supports int, int64 and string entity ids
    
    commit-id:d9d8e68d

--- interface.go
+++ interface.go
@@ -4,11 +4,20 @@ import (
 	"context"
 )
 
-// Browser is implemented by ent to allow for browsing all the
-//  entities in the ent schema.
+// Browser is implemented by ent to allow for browsing all the entities in the ent schema.
 type Browser interface {
+	// Types returns the name of all the ent schema types
 	Types() []string
+
+	// ConvertID converts a string id to the native golang type
+	ConvertID(typeName string, id string) interface{}
+
+	// Count returns the number of entities of a particular type
 	Count(ctx context.Context, entityType string) int
+
+	// List returns a list of entities
 	List(ctx context.Context, entityType string) interface{}
-	Object(ctx context.Context, typeName string, id int64) interface{}
+
+	// Object returns a particualr entity including all it's edges
+	Object(ctx context.Context, typeName string, id interface{}) interface{}
 }
